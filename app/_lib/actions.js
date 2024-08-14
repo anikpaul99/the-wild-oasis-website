48c5277bb7567467e86cd1b48400c0f2c3015791
Anik Paul <132483203+anikpaul99@users.noreply.github.com>
2024-08-14 18:52:19 +0600

    :sparkles: feat: delete an upcoming reservation

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -3,6 +3,7 @@
 import { auth, signIn, signOut } from "./auth";
 import { supabase } from "./supabase";
 import { revalidatePath } from "next/cache";
+import { getBookings } from "./data-service";
 
 /**
  * Update the guests profile.
@@ -29,6 +30,31 @@ export async function updateGuest(formData) {
   if (error) throw new Error("Guest could not be updated");
 
   revalidatePath("/account/profile");
+}
+
+/**
+ * To be responsible to delete the 'upcoming' reservation.
+ * @param {string} bookingId The 'id' - related to the particular reservation/booking which should be deleted.
+ * @author Anik Paul
+ */
+export async function deleteReservation(bookingId) {
+  const session = await auth();
+  if (!session) throw new Error("You must be logged in");
+
+  const guestBookings = await getBookings(session.user.guestId);
+  const guestBookingsIds = guestBookings.map((booking) => booking.id);
+
+  if (!guestBookingsIds.includes(bookingId))
+    throw new Error("You are not allowed to delete this booking");
+
+  const { error } = await supabase
+    .from("bookings")
+    .delete()
+    .eq("id", bookingId);
+
+  if (error) throw new Error("Booking could not be deleted");
+
+  revalidatePath("/account/reservations");
 }
 
 export async function signInAction() {
